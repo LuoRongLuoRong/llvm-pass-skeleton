383f22c7e3dc75ae372ec0ae591a63041a5d2ded
LuoRongLuoRong <17302010081@fudan.edu.cn>
2022-08-16 14:17:20 +0800

    fight against JIT but fail

--- run_demo.sh
+++ run_demo.sh
@@ -1,6 +1,7 @@
 export LLVM_DIR=/home/fdse/node_modules/llvm-13.0.0.obj/
 
 echo "(INFO) Start compiling the whole project..."
+mkdir build
 cd ./build
 rm -rf *
 cmake ..
@@ -11,15 +12,28 @@ src_dir='../src'
 pass_so_path='./skeleton/libSkeletonPass.so'
 
 clang++ -fPIC -emit-llvm -O0 -g -fno-discard-value-names -S -c ${src_dir}/demo.cpp -o ${src_dir}/demo.ll
-touch aaa.txt
-objdump -T ${pass_so_path} | grep logint
+# touch aaa.txt
+# objdump -T ${pass_so_path} | grep logint
 # objdump -T ${pass_so_path} > aaa.txt
 
 echo "(INFO) Start loading Pass..."
-opt -O0 -load-pass-plugin ${pass_so_path} --passes="skeleton" ${src_dir}/demo.ll -o demo
+opt -O0 -load-pass-plugin ${pass_so_path} --passes="skeleton" ${src_dir}/demo.ll -o demo.bin
 
 echo "(INFO) Finish loading Pass!"
 
-echo "(INFO) Start compiling codes..."
-lli -O0 demo
-echo "(INFO) Finish compiling codes!"
+# echo "(INFO) Start compiling codes..."
+# lli -O0 demo
+# echo "(INFO) Finish compiling codes!"
+
+echo "(INFO) Start generating object files..."
+llc -filetype=obj demo.bin 
+echo "(INFO) Finish generating object files!"
+
+
+echo "(INFO) Start generating "
+
+rtlib_dir='../runtimelib'
+g++ -fPIC -c ${rtlib_dir}/rtlib.cpp
+
+g++ -fPIC demo.bin.o rtlib.o
+echo "(INFO) Finish"
---
+++ runtimelib/rtlib.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <ctype.h>
+#include <stdio.h>
+#include <time.h>
+#include <fstream> //操作文件的头文件
+
+#define MAX_INT (((unsigned int)(-1)) >> 1)
+
+// write to file
+extern "C" void write2file(char *filename, int line, char *name, int type, const char *state, const char *old_state)
+{
+  const char *SEPARATOR = ", ";
+  const char *END_LINE = "\n";
+
+  // 定义流对象
+  std::ofstream ofs;
+  // 以追加写的方式打开文件
+  ofs.open("results.txt", std::ios::out | std::ios::app);
+
+  ofs << filename << SEPARATOR
+      << line << SEPARATOR
+      << name << SEPARATOR
+      << type << SEPARATOR
+      // << old_state << SEPARATOR
+      << state << END_LINE;
+
+  ofs.close();
+}
+
+// instrumented function
+
+extern "C" void logint(char *filename, int line, char *name, int type, int state, int old_state)
+{
+  std::cout << "Line " << line << ": " << name << " = " << state << "." << std::endl;
+  //  loglinevarint(line, name, state, old_state);
+  std::string s = std::to_string(state);
+  const char *pchar = s.c_str();
+  write2file(filename, line, name, type, pchar, pchar);
+}
+
+extern "C" void logbool(char *filename, int line, char *name, int type, bool state, bool old_state)
+{
+  std::cout << "Line " << line << ": " << name << " = " << state << "." << std::endl;
+  char const *pchar = state ? "1" : "0";
+  write2file(filename, line, name, type, pchar, pchar);
+}
+
+extern "C" void logchar(char *filename, int line, char *name, int type, char state, char old_state)
+{
+  if (state == 1 || state == 0)
+  {
+    logbool(filename, line, name, type, state, old_state);
+    return;
+  }
+  char pchar[] = "";
+  pchar[0] = state;
+
+  write2file(filename, line, name, type, pchar, pchar);
+}
+
+extern "C" void logstring(char *filename, int line, char *name, int type, char const *state, char const *old_state)
+{
+  std::cout << "Line " << line << ": " << name << " = " << state << "." << std::endl;
+  //    write2file(filename, line, name, state, old_state);
+}
+
+extern "C" void logchararray(char *filename, int line, char *name, int type, char const *state, char const *old_state)
+{
+  std::cout << "Line " << line << ": " << name << " = " << state << "." << std::endl;
+  write2file(filename, line, name, type, state, old_state);
+}
+
+extern "C" void logcharasterisk(char *filename, int line, char *name, int type, const char *state, char const *old_state)
+{
+  std::cout << "Line " << line << ": " << name << " = " << state << "." << strlen(state) << std::endl;
+  // 32~126号 是可打印字符
+  for (int i = 0; i < strlen(state); i++)
+  {
+    if (!isprint(state[i]))
+    {
+      const char *pchar = "";
+      write2file(filename, line, name, type, pchar, pchar);
+      return;
+    }
+  }
+  write2file(filename, line, name, type, state, state);
+}
+
+extern "C" void logdouble(char *filename, int line, char *name, int type, double state, double old_state)
+{
+  std::string s = std::to_string(state);
+  // std::cout << "测试 double " << state << std::endl;
+  char const *pchar = s.c_str();
+  write2file(filename, line, name, type, pchar, pchar);
+}
+
+extern "C" void logfloat(char *filename, int line, char *name, int type, float state, float old_state)
+{
+  std::string s = std::to_string(state);
+  // std::cout << "测试 float " << state << std::endl;
+  char const *pchar = s.c_str();
+  write2file(filename, line, name, type, pchar, pchar);
+}
+
+extern "C" void loglinevarint(int line, char *name, int state, int old_state)
+{
+  //  文件名，行号，变量，时间，前值，后值
+  std::cout << "BEFORE: Line " << line << ": " << name << " = " << old_state << "." << std::endl;
+  std::cout << "AFTER:  Line " << line << ": " << name << " = " << state << "." << std::endl;
+}
